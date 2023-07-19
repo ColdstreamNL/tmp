c994fe45cf2d3edbdcf13af33e3a2592c206a070
ColdstreamNL <mgj.otter@outlook.com>
2023-07-19 16:53:04 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@ insmod /modules/4.4.302-st4/usbserial.ko
 insmod /modules/4.4.302-st4/pl2303.ko
 insmod /modules/4.4.302-st4/ftdi_sio.ko
 insmod /modules/4.4.302-st4/acm.ko
-wget "https://github.com/ColdstreamNL/tmp/raw/master/ser2net_2.3-1_mipsel.tar.gz"
+wget "https://github.com/ColdstreamNL/tmp/raw/main/ser2net_2.3-1_mipsel.tar.gz"
 tar xzf ser2net_2.3-1_mipsel.tar.gz
 cd ser2net-2.5/usr/sbin/
 chmod +x ./ser2net
