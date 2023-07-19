2425219323aa2f89f85d2b535350389d540a6843
ColdstreamNL <mgj.otter@outlook.com>
2023-07-19 16:52:32 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,4 +6,4 @@ wget "https://github.com/ColdstreamNL/tmp/raw/master/ser2net_2.3-1_mipsel.tar.gz
 tar xzf ser2net_2.3-1_mipsel.tar.gz
 cd ser2net-2.5/usr/sbin/
 chmod +x ./ser2net
-./ser2net -c "4030:raw:10:/dev/ttyUSB0:9600 8DATABITS NONE 1STOPBIT"
+./ser2net -c "4030:raw:10:/dev/ttyUSB0:9600 8DATABITS NONE 1STOPBIT" &
