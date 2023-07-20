2110ad418010c8d334ff274537fc17705649e5af
ColdstreamNL <mgj.otter@outlook.com>
2023-07-20 23:57:49 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,10 @@
-insmod /modules/4.4.302-st4/usbserial.ko
-insmod /modules/4.4.302-st4/pl2303.ko
-insmod /modules/4.4.302-st4/ftdi_sio.ko
-insmod /modules/4.4.302-st4/acm.ko
-wget "https://github.com/ColdstreamNL/tmp/raw/main/ser2net_2.3-1_mipsel.tar.gz"
-tar xzf ser2net_2.3-1_mipsel.tar.gz
-cd ser2net-2.5/usr/sbin/
-chmod +x ./ser2net
-./ser2net -c "4030:raw:10:/dev/ttyUSB0:9600 8DATABITS NONE 1STOPBIT" &
+modprobe pl2303
+modprobe ftdi_sio
+mount /jffs/ /opt
+cd /opt
+curl -kLO https://raw.githubusercontent.com/Mateusz-Dera/DD-WRT-Easy-Entware-Installer/master/install.sh
+sh ./install.sh
+opkg install http://bin.entware.net/armv7sf-k3.2/archive/ser2net_2.10.0-2a_armv7-3.2.ipk
+echo 4030:raw:0:/dev/ttyUSB0:9600 8DATABITS NONE 1STOPBIT >> 1.conf
+echo 11880:UDP:0:/dev/ttyUSB0:9600 8DATABITS NONE 1STOPBIT >> 1.conf
+ser2net -c /opt/1.conf &
